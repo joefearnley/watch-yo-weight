8dfe263cb933ba0a4ae8f30ef274dceb445aa593
Joe Fearnley <joe.fearnley@gmail.com>
2016-02-28 16:14:48 -0500

    Removing laravel

--- app/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('/', 'HomeController@index');
-Route::get('/weight/all', 'WeightController@all');
-Route::post('/weight/add', 'WeightController@add');
-Route::post('/weight/delete', 'WeightController@delete');
-Route::get('/weight/chart-data', 'WeightController@chartData');
