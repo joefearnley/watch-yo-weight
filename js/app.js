d5b72aefd29326dfcce9ecf2635721050e703f2a
Joe Fearnley <joe.fearnley@gmail.com>
2016-03-01 22:30:19 -0500

    extracting out views

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function () {
+    var app = angular.module('weighIn');
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeController'
+            })
+            .when('/list', {
+                templateUrl: 'views/list',
+                controller: 'ListController'
+            })
+            .when('/add', {
+                templateUrl: 'views/add-weight.html',
+                controller: 'WeightController'
+            });
+    });
+
+
+    app.controller('HomeController', function() {
+
+        new Chartist.Line('.ct-chart', {
+            labels: response.dates,
+            series: [response.weights]
+        }, {
+            low: 150
+        });
+
+        $('#loading').hide();
+        $('#chart').show();
+    });
+
+
+
+})();
