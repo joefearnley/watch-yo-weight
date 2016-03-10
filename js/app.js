bc29cf72ff0d86beb2b91e12bdfb36ced8490e20
Joe Fearnley <joe.fearnley@gmail.com>
2016-03-09 22:33:53 -0500

    adding firebase components

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('weighIn', ['ngRoute', 'ngAnimate']);
+    var app = angular.module('weighIn', ['ngRoute', 'ngAnimate', 'firebase']);
 
     app.config(function($routeProvider) {
         $routeProvider
@@ -17,10 +17,7 @@
             });
     });
 
-
     app.controller('HomeController', function($scope) {
-
-        $scope.pageClass = 'page-home';
 
         //new Chartist.Line('.ct-chart', {
         //    labels: response.dates,
@@ -33,11 +30,53 @@
         //$('#chart').show();
     });
 
-    app.controller('ListController', function($scope) {
-        $scope.pageClass = 'page-list';
+    app.controller('ListController', function($scope, $firebaseObject) {
+        //$scope.weights = getData();
+
+        var rootRef =
     });
 
     app.controller('WeightController', function($scope) {
         $scope.pageClass = 'page-add';
     });
+
+    function getData() {
+        return [{
+            "date": '2016-01-03',
+            "weight": 183.00
+        }, {
+            "date": '2016-01-09',
+            "weight": 180.00
+        }, {
+            "date": '2016-01-16',
+            "weight": 178.00
+        }, {
+            "date": '2016-01-29',
+            "weight": 177.00
+        }, {
+            "date": '2016-02-06',
+            "weight": 173.50
+        }, {
+            "date": '2016-02-09',
+            "weight": 175.00
+        }, {
+            "date": '2016-02-12',
+            "weight": 173.00
+        }, {
+            "date": '2016-02-18',
+            "weight": 172.00
+        }, {
+            "date": '2016-02-20',
+            "weight": 170.00
+        }, {
+            "date": '2016-02-26',
+            "weight": 169.60
+        }, {
+            "date": '2016-03-03',
+            "weight": 172.30
+        }, {
+            "date": '2016-03-08',
+            "weight": 175.30
+        }];
+    }
 })();
