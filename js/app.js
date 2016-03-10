06d8fb4df541b0fde2313b0a09e41b347be5cff7
Joe Fearnley <joe.fearnley@gmail.com>
2016-03-09 21:32:57 -0500

    Correcting view names and animate library

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-(function () {
-    var app = angular.module('weighIn');
+(function() {
+    var app = angular.module('weighIn', ['ngRoute', 'ngAnimate']);
 
     app.config(function($routeProvider) {
         $routeProvider
@@ -8,26 +8,36 @@
                 controller: 'HomeController'
             })
             .when('/list', {
-                templateUrl: 'views/list',
+                templateUrl: 'views/list.html',
                 controller: 'ListController'
             })
             .when('/add', {
-                templateUrl: 'views/add-weight.html',
+                templateUrl: 'views/add.html',
                 controller: 'WeightController'
             });
     });
 
 
-    app.controller('HomeController', function() {
+    app.controller('HomeController', function($scope) {
 
-        new Chartist.Line('.ct-chart', {
-            labels: response.dates,
-            series: [response.weights]
-        }, {
-            low: 150
-        });
+        $scope.pageClass = 'page-home';
 
-        $('#loading').hide();
-        $('#chart').show();
+        //new Chartist.Line('.ct-chart', {
+        //    labels: response.dates,
+        //    series: [response.weights]
+        //}, {
+        //    low: 150
+        //});
+        //
+        //$('#loading').hide();
+        //$('#chart').show();
+    });
+
+    app.controller('ListController', function($scope) {
+        $scope.pageClass = 'page-list';
+    });
+
+    app.controller('WeightController', function($scope) {
+        $scope.pageClass = 'page-add';
     });
 })();
