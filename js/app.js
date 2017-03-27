cf807f417913dd3aad69480514dd1908425b2ecd
Joe Fearnley <joe.fearnley@gmail.com>
2017-03-27 21:00:09 +0000

    Adding total time and weight

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 axios.get('data/weights.json')
     .then(function(response) {
-        var dates = [];
-        var weights = [];
+        let hightestWeight = 0;
+        let dates = [];
+        let weights = [];
         for (let key in response.data) {
-            if(response.data.hasOwnProperty(key)) {
-                var dateParts = response.data[key].date.split('-');
+            if (response.data.hasOwnProperty(key)) {
+                let dateParts = response.data[key].date.split('-');
                 dates.push(dateParts[1] + '/' + dateParts[2] + "/" + dateParts[0]);
                 weights.push(response.data[key].weight);
+
+                if (response.data[key].weight > hightestWeight) {
+                    hightestWeight = response.data[key].weight;
+                }
             }
         }
 
@@ -21,4 +26,17 @@ axios.get('data/weights.json')
                 }]
             }
         });
+
+        const latestWeight = hightestWeight - weights[weights.length - 1];
+        const totalWeightLoss = document.querySelector('#total-weight-loss');
+        totalWeightLoss.innerHTML = latestWeight.toFixed(2) + ' lbs';
+        const totalTime = document.querySelector('#total-time');
+
+        let firstDate = new Date(dates[0]);
+        let latestDate = new Date(dates[dates.length - 1]);
+
+        let months = (latestDate.getMonth() + 12 * latestDate.getFullYear()) 
+            - (firstDate.getMonth() + 12 * firstDate.getFullYear());
+        
+        totalTime.innerHTML = (months / 12).toFixed(1) + ' years';
     });
